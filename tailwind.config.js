486f8f21d9bc124d1b19e6b0c4a371a69f608e4a
Javier Blanco García-Lomas <jblancogl@gmail.com>
2019-11-27 09:54:15 +0100

    change theme.screens.lg & xl to 768px

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,11 @@
 module.exports = {
   theme: {
+    screens: {
+      sm: '640px',
+      md: '768px',
+      lg: '768px',
+      xl: '768px',
+    },
     container: {
       center: true,
     },
