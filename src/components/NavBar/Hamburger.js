4140a89d7c0f04b244024bb0b16cee860ce98d3f
Javier Blanco García-Lomas <jblancogl@gmail.com>
2019-11-28 01:56:58 +0100

    added src folder

---
+++ src/components/NavBar/Hamburger.js
@@ -0,0 +1,22 @@
+const Hamburger = props => (
+  <div>
+    <svg
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="#000"
+      style={{ verticalAlign: 'middle' }}
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <g className="nc-icon-wrapper">
+        <path d="M3 10h18" />
+        <path d="M3 18h18" />
+      </g>
+    </svg>
+  </div>
+)
+
+export default Hamburger
--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,10 +1,12 @@
 import Brand from './Brand'
 import Menu from './Menu'
+import Hamburger from './Hamburger'
 
 const Nav = () => (
   <nav className="fixed top-0 left-0 right-0 bg-white opacity-75 p-2 mb-5 border-b border-gray-400 flex">
     <Brand />
-    <Menu />    
+    <Menu />
+    <Hamburger />
   </nav>
 )
 
