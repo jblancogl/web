50cf0fd266335d60fd69c17f64cb2d664fb8376a
Javier Blanco García-Lomas <jblancogl@gmail.com>
2019-11-28 02:38:30 +0100

    add purgecss to project

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,19 @@
-const tailwindcss = require('tailwindcss')
-const autoprefixer = require('autoprefixer')
+const purgecss = require('@fullhuman/postcss-purgecss')({
+  content: [
+    './src/components/**/*.js',
+    './src/pages/**/*.js',
+    './src/layouts/**/*.js',
+    './src/content/**/*.mdx'
+  ],
+  defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || [],
+  whitelist: ['html', 'body'],
+})
 
 module.exports = {
   plugins: [
-    tailwindcss,
-    autoprefixer
+    require('postcss-import'),
+    require('tailwindcss'),
+    require('postcss-preset-env')({ stage: 1 }),
+    ...(process.env.NODE_ENV === 'production' ? [purgecss] : []),
   ],
 }
