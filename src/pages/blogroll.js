d065823ea88261325fbf2f94ac14fc0f051b97c1
Javier Blanco García-Lomas <jblancogl@gmail.com>
2020-01-06 16:29:21 +0100

    improved card profile person on blogroll page

--- src/pages/blogroll.js
+++ src/pages/blogroll.js
@@ -37,12 +37,12 @@ const card = (props) => {
           className="rounded-full"
           src={`https://avatars.io/twitter/${props.twitter}/medium`}
           alt={`twitter image profile of ${props.twitter}/medium`}
-          width="100"
-          height="100"
+          width="100px"
+          height="100px"
           loading="lazy" />
-        <div class="ml-3">
+        <div class="ml-3 flex flex-col justify-center">
           <h2>{props.title}</h2>
-          <a href={props.link}>{props.link}</a>
+          <a className="text-base text-red-600" href={props.link}>{props.link}</a>
         </div>
       </div>
       <p>{props.description}</p>
@@ -54,8 +54,10 @@ export default () => (
   <Page title={title}>
     <h1>Blogroll</h1>
     <h2>Other people on the web</h2>
+    <hr className="my-6" />
     <ul className="flex flex-wrap -mx-3 -mb-3">
       {people.map(person => card(person))}
     </ul>
+    <hr className="my-6" />
   </Page>
 )
