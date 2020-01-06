5f3bbd760b4db15bac8df10083cfd399bb1316f5
Javier Blanco García-Lomas <jblancogl@gmail.com>
2020-01-06 17:54:13 +0100

    @atinux, @AlbertoCairo y @david_bonilla added to the blogroll list

--- src/pages/blogroll.js
+++ src/pages/blogroll.js
@@ -26,34 +26,56 @@ const people = [
     link: 'https://brandur.org',
     twitter: 'brandur',
     description: ''
+  },
+  {
+    title: 'Sébastien Chopin',
+    link: 'https://atinux.com',
+    twitter: 'Atinux',
+    description: ''
+  },
+  {
+    title: 'Alberto Cairo',
+    link: 'http://thefunctionalart.com',
+    twitter: 'AlbertoCairo',
+    description: ''
+  },
+  {
+    title: 'David Bonilla',
+    link: 'https://bonillaware.com',
+    twitter: 'david_bonilla',
+    description: ''
   }
 ]
 
 const card = (props) => {
   return (
-    <li className="px-3 mb-3 lg:w-1/3">
+    <li key={props.twitter} className="px-3 mb-3 lg:w-1/3">
       <div className="flex">
-        <div className="w-16 h-16">
+        <div style={{
+          minWidth: '48px'
+        }}>
           <img
-            className="rounded-full w-full h-full"
-            src={`https://avatars.io/twitter/${props.twitter}/medium`}
-            alt={`twitter image profile of ${props.twitter}/medium`}
+            className="rounded-full"
+            width="48px"
+            height="48px"
+            src={`https://avatars.io/twitter/${props.twitter}/small`}
+            alt={`twitter image profile of ${props.twitter}`}
             loading="lazy" />
         </div>
-        <div class="ml-3 flex flex-col justify-center">
-          <h2 class="lg:text-base">{props.title}</h2>
-          <a className="text-base text-red-600" href={props.link}>{props.link}</a>
+        <div className="ml-3 flex flex-col justify-center">
+          <h2 className="lg:text-base">{props.title}</h2>
+          <a className="text-base text-red-600 overflow-hidden" href={props.link}>{props.link}</a>
         </div>
       </div>
       <p>{props.description}</p>
-    </li>
+    </li >
   )
 }
 
 export default () => (
   <Page title={title}>
-    <h1>Blogroll</h1>
-    <h2>Other people on the web</h2>
+    <h1 className="lg:text-6xl lg:text-center">Blogroll</h1>
+    <h2 className="lg:text-3xl lg:text-center">Other people on the web</h2>
     <hr className="my-6" />
     <ul className="flex flex-wrap -mx-3 -mb-3">
       {people.map(person => card(person))}
