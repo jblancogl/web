510d2df7f2246c0b6b642778574d67bb75db592f
Javier Blanco García-Lomas <jblancogl@gmail.com>
2020-01-07 12:18:26 +0100

    autogenerate csv data on public/data folder

---
+++ scripts/generate_data.sh
@@ -0,0 +1,6 @@
+#! /usr/bin/env bash
+rm public/data/*.csv |
+ls public/data/*.json | 
+xargs -I dir basename dir | 
+cut -f 1 -d '.' | 
+xargs -I name json2csv -i public/data/name.json -o public/data/name.csv
